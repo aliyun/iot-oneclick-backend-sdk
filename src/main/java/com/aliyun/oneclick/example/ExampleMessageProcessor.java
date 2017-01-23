8d2951486f835673700a0bd67a15c0233505ea4d
卢镗 <zhirui.rzr@alibaba-inc.com>
2017-01-23 15:26:22 +0800

    more comments

--- src/main/java/com/aliyun/oneclick/example/ExampleMessageProcessor.java
+++ src/main/java/com/aliyun/oneclick/example/ExampleMessageProcessor.java
@@ -8,25 +8,37 @@ import com.aliyun.oneclick.lib.MessageProcessor;
  * Created by zhirui.rzr on 2017/1/23.
  */
 public class ExampleMessageProcessor implements MessageProcessor {
+    /**
+     * 收到有效按键消息时调用
+     * 开发者可实现此方法，在其中处理业务逻辑
+     * @param seq 消息序列号（SDK已做去重处理，开发者可以忽略）
+     * @param button 按键值（Button枚举型）
+     * @param serial 按键序列号
+     * @return 期望亮的灯（Light枚举型）
+     */
     @Override
-    public Light process(int seq, Button status, String serial) {
-        switch (status) {
-            case CLICK:
+    public Light process(int seq, Button button, String serial) {
+        switch (button) { // 判断按键值
+            case CLICK: // 收到单击按键
                 System.out.println(String.format("user clicked the button %s", serial));
-                return Light.GREEN;
-            case DOUBLE_CLICK:
+                return Light.GREEN; // 亮绿灯
+            case DOUBLE_CLICK: // 收到双击按键
                 System.out.println(String.format("user double click the button %s", serial));
-                return Light.MIX_BLINK;
-            case LONG_CLICK:
+                return Light.MIX_BLINK; // 红绿混合闪烁
+            case LONG_CLICK:  // 收到长按
                 System.out.println(String.format("user long clicked the button %s", serial));
-                return Light.GREEN_BLINK;
+                return Light.GREEN_BLINK; // 绿灯闪烁
         }
         System.out.println("something wrong happened");
-        return Light.RED;
+        return Light.RED; // 其它情况亮红灯
     }
 
+    /**
+     * 出错时亮红灯
+     * @return Light.RED
+     */
     @Override
     public Light errorLight() {
-        return Light.RED;
+        return Light.RED; // 红灯
     }
 }
--- src/main/java/com/aliyun/oneclick/lib/MessageProcessor.java
+++ src/main/java/com/aliyun/oneclick/lib/MessageProcessor.java
@@ -4,6 +4,19 @@ package com.aliyun.oneclick.lib;
  * Created by zhirui.rzr on 2017/1/22.
  */
 public interface MessageProcessor {
-    Light process(int seq, Button status, String serial);
+    /**
+     * 收到有效按键消息时调用
+     * 开发者可实现此方法，在其中处理业务逻辑
+     * @param seq 消息序列号（SDK已做去重处理，开发者可以忽略）
+     * @param button 按键值（Button枚举型）
+     * @param serial 按键序列号
+     * @return 期望亮的灯（Light枚举型）
+     */
+    Light process(int seq, Button button, String serial);
+
+    /**
+     * SDK处理出错时，开发者期望亮的灯
+     * @return 亮灯值，Light枚举型
+     */
     Light errorLight();
 }
